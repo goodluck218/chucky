987c1381b70d23073e94cc0a7334d1754629544d
goodluck218 <goodluckmaduabuchi92@gmail.com>
2024-09-27 23:13:53 +0100

    Add files via upload

---
+++ Chucky/settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['!','.',',','🐤','🗿']
+
+global.owner = ['2348108778025']
+global.botname = 'Chucky V5 Beta'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "King Marx"
+global.sticker2 = "👑"
