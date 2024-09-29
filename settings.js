b3122581efdf3eb7c7c1cea0d91a0f237df57552
goodluck218 <goodluckmaduabuchi92@gmail.com>
2024-09-29 09:14:45 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['!','.',',','🐤','🗿']
 
-global.owner = ['2348108778025']
+global.owner = ['2349071634600']
 global.botname = 'Chucky V5 Beta'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "King Marx"
